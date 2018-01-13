2e4042b4f63c873dadf64e4a0f90bfe089bd9dcf
tholiv <thiago_oliveira@amp.com.au>
2018-01-13 19:05:54 +1100

    Removing some dependencies.

--- handlers/home.go
+++ handlers/home.go
@@ -9,21 +9,23 @@ import (
 )
 
 // home is a simple HTTP handler function which writes a response.
-func home(w http.ResponseWriter, _ *http.Request) {
-	info := struct {
-		BuildTime string `json:"buildTime"`
-		Commit    string `json:"commit"`
-		Release   string `json:"release"`
-	}{
-		version.BuildTime, version.Commit, version.Release,
-	}
+func home(buildTime, commit, release string) http.HanlderFunc {
+  return func(w http.ResponseWriter, _ *http.Request) {
+    info := struct {
+      BuildTime string `json:"buildTime"`
+      Commit    string `json:"commit"`
+      Release   string `json:"release"`
+    }{
+      version.BuildTime, version.Commit, version.Release,
+    }
 
-	body, err := json.Marshal(info)
-	if err != nil {
-		log.Printf("Could not encode info data: %v", err)
-		http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(body)
+    body, err := json.Marshal(info)
+    if err != nil {
+      log.Printf("Could not encode info data: %v", err)
+      http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+      return
+    }
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(body)
+  }
 }
